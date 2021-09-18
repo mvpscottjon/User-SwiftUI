84c90ee93d438cc17d1a279e7995911cde41cdd0
Seven <mvpscottjon@gmail.com>
2021-09-18 10:27:35 +0800

    init

---
+++ UserSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserSwiftUI
+//
+//  Created by Seven on 2021/9/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
